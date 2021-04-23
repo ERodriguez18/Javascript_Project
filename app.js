be84f8fd46f4ecdafc3678f4b08a8a8554c9fe14
Erick <erisrnb18@gmail.com>
2021-04-23 15:51:34 -0400

    added function to randomize location of correct answer

--- app.js
+++ app.js
@@ -65,3 +65,15 @@ function createAnswers(answers){
     answer = randomizeArray(answer);
     return answer;
 }
+
+function randomizeArray(array){
+    for (let i = array.length-1; i >=0; i--) {
+     
+        let randomIndex = Math.floor(Math.random()*(i+1)); 
+        let itemAtIndex = array[randomIndex]; 
+         
+        array[randomIndex] = array[i]; 
+        array[i] = itemAtIndex;
+    }
+    return array;
+}
