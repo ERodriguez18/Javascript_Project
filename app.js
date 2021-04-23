88c9428141f691a21191f3c857667c8c139efc3c
Erick <erisrnb18@gmail.com>
2021-04-23 13:00:17 -0400

    added event listener to call api fetch function

--- app.js
+++ app.js
@@ -6,3 +6,25 @@ let   scoreChange = document.querySelector('.score--change');
 const winner = document.querySelector('.winner');
 const replayBtn = document.querySelector('.btn--yes');
 const playerName = document.querySelector('.player'); 
+
+submit.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    getMeSomeQuestions();
+    quizControls.style.display = 'none';
+
+    playerPrompt();
+    
+});
+
+function getMeSomeQuestions(){
+    const url = 'https://opentdb.com/api.php?amount=10&category=11&difficulty=medium&type=multiple';
+
+    const fetchQuestionsAwait = async () => {
+        const response = await fetch(url)
+        const data = await response.json();
+        createQuestions(data.results);
+    
+    }
+    fetchQuestionsAwait();
+}
