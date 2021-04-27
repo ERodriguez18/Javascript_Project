d55630fa51893793de78f902d6dc489f63698fdc
Erick <erisrnb18@gmail.com>
2021-04-26 20:14:24 -0400

    Updated mobile view

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const submit = document.querySelector('#form');
 const questionContainer = document.querySelector('.questionContainer');
-const quizControls = document.querySelector('#quizControls');
+const triviaControls = document.querySelector('#triviaControls');
 const totalQuestions = document.querySelector('.score--total');
 let   scoreChange = document.querySelector('.score--change');
 const winner = document.querySelector('.winner');
@@ -11,7 +11,7 @@ submit.addEventListener('submit', function(e) {
     e.preventDefault();
 
     getMeSomeQuestions();
-    quizControls.style.display = 'none';
+    triviaControls.style.display = 'none';
 
     playerPrompt();
     
